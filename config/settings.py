322b11978f768a20ca070dde0a77a9ffbfa87bf9
kamula <kamulaisaac@gmail.com>
2025-08-12 12:08:17 +0300

    customers app

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "auth",
+
+    "customers",
     "categories",
     "products",
     "orders",
