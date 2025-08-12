0b16efacdbaac6eea61b0be731f60bb948bd0770
kamula <kamulaisaac@gmail.com>
2025-08-12 10:59:11 +0300

    project apps

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    
+    "auth",
+    "categories",
+    "products",
+    "orders",
 ]
 
 MIDDLEWARE = [
