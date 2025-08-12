da362f845edcb6627e3473fd6610f8532a1de461
kamula <kamulaisaac@gmail.com>
2025-08-12 14:55:07 +0300

    products model time update

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.utils import timezone
 
 from categories.models import Category
 
@@ -12,6 +13,8 @@ class Product(models.Model):
     )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
