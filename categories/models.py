d3e57988e7d93d33d4bef523f7ef8f0b064ad843
kamula <kamulaisaac@gmail.com>
2025-08-12 12:17:28 +0300

    product model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return self.name
--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-
+    "mptt",
+    
     "customers",
     "categories",
     "products",
