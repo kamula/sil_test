eaa250a27aa5400f95459a87ed7ced249d56bb4b
kamula <kamulaisaac@gmail.com>
2025-08-12 12:03:34 +0300

    customer model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
