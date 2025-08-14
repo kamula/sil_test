e37891696f885b892662da84af9a9d4682bd7950
kamula <kamulaisaac@gmail.com>
2025-08-14 12:30:07 +0300

    secret key

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-pipx6!k!5q@2ou(&5g&&v81r8lm$ls82@f23#vii@+egyiqu_u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
