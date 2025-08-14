011e74324e3ad0713feda242b8c9cd20bc3d585c
kamula <kamulaisaac@gmail.com>
2025-08-14 15:12:31 +0300

    settings update

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-default-key-for-testing")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
